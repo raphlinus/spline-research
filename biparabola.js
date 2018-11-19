3ccb19ca62977f7d5525aa11ddba26f81534fb86
Raph Levien <raph.levien@gmail.com>
2018-11-19 08:46:07 -0800

    Initial commit of spline sketchbook
    This is a repo for experimentation inspired by "κ-Curves:
    Interpolation at Local Maximum Curvature". The first thing I'm going
    to do is represent the "biparabola" as a two paramater curve family.

---
+++ biparabola.js
@@ -0,0 +1,3 @@
+var e = document.getElementById("spiral");
+console.log(e);
+e.setAttribute("d", "M10 240 L630 400");
---
+++ locality.py
@@ -0,0 +1,30 @@
+# Computation of locality ratio for G3, kappa'=0 case
+
+def ratio_g3(a):
+	# kappa = a - (1 + a) * (3x^2 - 2x^3)
+	# th = ax - (1 + a) * (x^3 - 1/2 x^4)
+	# y = 1/2 a x^2 - (1 + a) * (1/4 x^4 - 1/10 x^5)
+	y1 = 0.5 * a - (1 + a) * 0.15
+	z0 = -y1
+	z1 = z0 + a - (1 + a) * 0.5
+	print a, -z0 / z1
+
+if False:
+	for i in range(10):
+		a = 4.44151 + i * 0.000001
+		ratio_g3(a)
+
+# k'=0 on high curvature side, k''=0 on low curvature side
+def ratio_asym(a):
+	# kappa = a - (1 + a) * x^2
+	# th = ax - (1 + a) * (1/3 x^3)
+	# y = 1/2 a x^2 - (1 + a) * (1/12 x^4)
+	y1 = 0.5 * a - (1 + a) * (1./12)
+	z0 = -y1
+	z1 = z0 + a - (1 + a) * (1./3)
+	print a, -z0 / z1
+
+if True:
+	for i in range(10):
+		a = 2.5 + i * .01
+		ratio_asym(a)
